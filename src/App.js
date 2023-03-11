addc10b80dacd950975a3268c261a5aa776413ee
sakil-ahmed <sakilahmed1495@gmail.com>
2023-03-11 23:17:19 +0600

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Home from "./container/Home";
 const App = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const User =
-      localStorage.getItem("user") !== "undefined"
-        ? JSON.parse(localStorage.getItem("user"))
-        : localStorage.clear();
-
-    if (!User) navigate("/login");
-  }, []);
-
   return (
     <Routes>
       <Route path="login" element={<Login />} />
